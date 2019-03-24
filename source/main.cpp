d27c873e8f13eb353f09b40e0d11ba888979adf5
Leo An <leo@innoaus.com>
2019-03-24 15:14:11 +0000

    add a cmake sample

---
+++ source/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "main.h"
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Welcome OnionIoT Omega World !!!" << std::endl;
+    return 0;
+}
---
+++ source/main.h
@@ -0,0 +1,5 @@
+#ifndef __main_h__
+#define __main_h__
+
+
+#endif // __main_h__
